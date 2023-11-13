f9e384b822f7d58e99136db74e66ff41651daf2f
javadebadi <javad.ebadi.1990@gmail.com>
2023-11-12 21:48:19 -0700

    Adds foundation terraform module

---
+++ foundation/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "~> 1.6.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5"
+      }
+  }
+}
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+}
+
+provider "google-beta" {
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone 
+}
---
+++ foundation/terraform.tfvars
@@ -0,0 +1,3 @@
+project_id = "nice-beanbag-402218"
+region = "us-central1"
+zone = "us-central1-a"
