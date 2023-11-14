e85fac2cbe7a96ea4b91f097395fe941ddb7f0f9
javadebadi <javad.ebadi.1990@gmail.com>
2023-11-13 21:37:06 -0700

    Updated output for subnets

--- foundation/output.tf
+++ foundation/output.tf
@@ -1,3 +1,9 @@
 output "network_self_link" {
   value = google_compute_network.this.self_link
 }
+
+output "subnetwork_self_links" {
+  value = {
+    for subnet in google_compute_subnetwork.this : subnet.name => subnet.self_link
+  }
+}
